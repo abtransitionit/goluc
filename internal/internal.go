f7cc0351ee26c3d6ae543bb54d3aeb1c2b90cc55
abelgacem <amarbelgacem@hotmail.fr>
2025-08-21 11:53:55 +0200

    add a dummy fn to internal

--- internal/internal.go
+++ internal/internal.go
@@ -18,47 +18,60 @@ var CliName = filepath.Base(os.Args[0])
 var logger = logx.GetLogger()
 
 // defines the functions used by a phase
-func SetupFunc(ctx context.Context, cmd ...string) (string, error) {
+// func ShowPhase(ctx context.Context, cmd ...string) (string, error) {
+// 	fmt.Println("  [SETUP] Performing initial setup...")
+// 	time.Sleep(500 * time.Millisecond) // Simulate work
+// 	return "Setup complete!", nil
+// }
+
+func SetupFunc(ctx context.Context, l logx.Logger, cmd ...string) (string, error) {
 	fmt.Println("  [SETUP] Performing initial setup...")
 	time.Sleep(500 * time.Millisecond) // Simulate work
 	return "Setup complete!", nil
 }
 
-func BuildFunc(ctx context.Context, cmd ...string) (string, error) {
+func BuildFunc(ctx context.Context, l logx.Logger, cmd ...string) (string, error) {
 	fmt.Println("  [BUILD] Compiling the application...")
 	time.Sleep(1 * time.Second) // Simulate work
 	return "Build successful!", nil
 }
 
-func TestFunc(ctx context.Context, cmd ...string) (string, error) {
+func TestFunc(ctx context.Context, l logx.Logger, cmd ...string) (string, error) {
 	fmt.Println("  [TEST] Running unit tests...")
 	time.Sleep(750 * time.Millisecond) // Simulate work
 	return "All tests passed!", nil
 }
 
 // Dummy Phase Functions (for demonstration purposes)
-func CheckSystemStatus(ctx context.Context, cmd ...string) (string, error) {
+func CheckSystemStatus(ctx context.Context, l logx.Logger, cmd ...string) (string, error) {
 	logger.Info("Checking system status...")
 	time.Sleep(1 * time.Second)
 	logger.Info("System status OK.")
 	return "System status OK", nil
 }
 
-func FetchLatestData(ctx context.Context, cmd ...string) (string, error) {
+func FetchLatestData(ctx context.Context, l logx.Logger, cmd ...string) (string, error) {
 	logger.Info("Fetching latest data...")
 	time.Sleep(2 * time.Second)
 	logger.Info("Data fetched successfully.")
 	return "Data fetched successfully", nil
 }
 
-func ProcessData(ctx context.Context, cmd ...string) (string, error) {
+func ProcessData(ctx context.Context, l logx.Logger, cmd ...string) (string, error) {
 	logger.Info("Processing data...")
 	time.Sleep(3 * time.Second)
 	logger.Info("Data processed.")
 	return "Data processed", nil
 }
 
-func GenerateReport(ctx context.Context, cmd ...string) (string, error) {
+func GenerateReport(ctx context.Context, l logx.Logger, cmd ...string) (string, error) {
+	logger.Info("Generating report...")
+	time.Sleep(1 * time.Second)
+	logger.Info("Report generated.")
+	return "Report generated", nil
+}
+
+func Dummy(ctx context.Context, l logx.Logger, cmd ...string) (string, error) {
 	logger.Info("Generating report...")
 	time.Sleep(1 * time.Second)
 	logger.Info("Report generated.")
