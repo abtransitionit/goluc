974a107985f64f352596b5d3f203744039a607cb
abelgacem <amarbelgacem@hotmail.fr>
2025-08-26 11:14:33 +0200

    upd

--- cmd/goprj/build.go
+++ cmd/goprj/build.go
@@ -4,20 +4,37 @@ Copyright © 2025 AB TRANSITION IT abtransitionit@hotmail.com
 package goprj
 
 import (
+	"github.com/abtransitionit/gocore/cli"
 	"github.com/abtransitionit/gocore/logx"
 	"github.com/spf13/cobra"
 )
 
-// Package variables for CLI flags
+// Package variables for CLI flags.
+var (
+	projectPath string
+	outputDir   string
+)
 
-// root command
+// the command
 var buildCmd = &cobra.Command{
 	Use:   "build",
-	Short: "build the artifact from a go project",
+	Short: "Build the artifcat of a Go project",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		logger := logx.GetLogger()
-		logger.Infof("%s", cmd.Short)
+		if err := cli.BuildGoProject(logger, projectPath, outputDir); err != nil {
+			logger.Errorf("%v", err)
+			return nil
+		}
 
 		return nil
 	},
+}
+
+func init() {
+	// Add flags to the command.
+	buildCmd.Flags().StringVarP(&projectPath, "path", "p", "", "Full path to the Go project to build")
+	buildCmd.Flags().StringVarP(&outputDir, "output-dir", "o", "/tmp", "Directory to save the built artifact")
+
+	// Mark the 'path' flag as required.
+	buildCmd.MarkFlagRequired("path")
 }
