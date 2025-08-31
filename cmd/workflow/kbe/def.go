a90ccc0975af873aa298a696fde8c20904b3f173
abelgacem <amarbelgacem@hotmail.fr>
2025-08-31 14:38:54 +0200

    update

--- cmd/workflow/kbe/def.go
+++ cmd/workflow/kbe/def.go
@@ -4,10 +4,12 @@ Copyright © 2025 AB TRANSITION IT abtransitionit@hotmail.com
 package kbe
 
 import (
+	coregocli "github.com/abtransitionit/gocore/gocli"
 	"github.com/abtransitionit/gocore/logx"
 	corephase "github.com/abtransitionit/gocore/phase"
 	"github.com/abtransitionit/goluc/internal"
 	"github.com/abtransitionit/gotask/dnfapt"
+	taskgocli "github.com/abtransitionit/gotask/gocli"
 	"github.com/abtransitionit/gotask/luc"
 	"github.com/abtransitionit/gotask/vm"
 )
@@ -29,6 +31,10 @@ var (
 var (
 	vmList                = []string{"o1u", "o2a", "o3r", "o4f", "o5d"}
 	listRequiredDaPackage = []string{"gnupg"} // gnupg/{gpg}
+	listGoCli             = []coregocli.GoCli{
+		{Name: "kind", Version: "latest"},
+	}
+
 	// listGoCli             = []string{"gocli"}
 )
 
@@ -45,7 +51,7 @@ func init() {
 		corephase.NewPhase("copyAgent", "copy LUC CLI agent to all VMs", luc.DeployLuc, []string{"checkVmAccess"}),
 		corephase.NewPhase("upgradeOs", "provision OS nodes with latest dnfapt packages and repositories.", dnfapt.UpgradeVmOs, []string{"copyAgent"}),
 		corephase.NewPhase("updateApp", "provision required/missing standard dnfapt packages.", dnfapt.UpdateVmOsApp(listRequiredDaPackage), []string{"upgradeOs"}),
-		// corephase.NewPhase("installGoCli", "provision Go CLI(s).", gocli.InstallOnVm(listGoCli), []string{"updateApp"}),
+		corephase.NewPhase("installGoCli", "provision Go CLI(s).", taskgocli.InstallOnVm(listGoCli), []string{"updateApp"}),
 		corephase.NewPhase("dapack2", "provision OS dnfapt package(s) on VM(s).", internal.CheckSystemStatus, []string{"installGoCli"}),
 		corephase.NewPhase("darepo", "provision dnfapt repositories.", internal.GenerateReport, []string{"installGoCli"}),
 	)
