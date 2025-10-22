8b0215212cc87643322fcffa3d5d4eafe7fa31be
abelgacem <amarbelgacem@hotmail.fr>
2025-10-22 16:27:30 +0200

    update

--- cmd/k8s/helm/chart/kind.go
+++ cmd/k8s/helm/chart/kind.go
@@ -40,94 +40,89 @@ var kindCmd = &cobra.Command{
   `, internal.CliName),
 	Run: func(cmd *cobra.Command, args []string) {
 
+		var helmChart helm.HelmChart
+		var helmRepo helm.HelmRepo
+
 		// define ctx and logger
 		logger := logx.GetLogger()
 		logger.Info(kindSDesc)
-
-		// // check parameters
-		// if chartPathFlag == "" {
-		// 	logger.Error("chart localisation is required (--path)")
-		// 	return
-		// }
 
 		// 1 - list kind if flag is set to use a chart on the FS
 		if chartPathFlag != "" {
 
 			// 11 - define object from the resource property
-			helmChart := helm.HelmChart{
+			helmChart = helm.HelmChart{
 				FullName: chartPathFlag,
 			}
 
-			// 12 - create the release
-			output, err := helm.ListChartKind(localFlag, "o1u", helmChart, logger)
+		} else {
+
+			// 2 - list kind using a chart in a repo configured in helm client
+			// 21 - get the list of helm repositories
+			output, err := helm.ListRepo(localFlag, "o1u", logger)
 			if err != nil {
-				logger.Errorf("failed to create helm release: %v", err)
+				logger.Errorf("failed to list helm repo: %v", err)
 				return
 			}
-			// success
+
+			// print the list
 			list.PrettyPrintTable(output)
 
-			// exit
-			return
-		}
+			// Ask user which ID (to choose) from the printed list
+			id, err := ui.AskUserInt("\nchoose repo (enter ID): ")
+			if err != nil {
+				logger.Errorf("invalid ID: %v", err)
+				return
+			}
 
-		// 2 - list kind using a chart in a repo configured in helm client
-		// 21 - get the list of helm repositories
-		output, err := helm.ListRepo(localFlag, "o1u", logger)
+			// define resource property from ID and output
+			repoName, err := list.GetFieldByID(output, id, 0)
+			if err != nil {
+				logger.Errorf("failed to get resource property from ID: %s: %v", id, err)
+				return
+			}
+
+			// define object from the resource property
+			helmRepo = helm.HelmRepo{Name: repoName}
+
+			// 22 - get the list of charts in this repo
+			output, err = helm.ListChart(localFlag, "o1u", helmRepo, logger)
+			if err != nil {
+				logger.Errorf("failed to list helm charts: %v", err)
+				return
+			}
+
+			// print the list
+			list.PrettyPrintTable(output)
+
+			// 23 - Ask user which ID (to choose) from the list
+			id, err = ui.AskUserInt("\nchoose chart (enter ID): ")
+			if err != nil {
+				logger.Errorf("invalid ID: %v", err)
+				return
+			}
+
+			// 24 - define resource property from ID and output
+			chartName, err := list.GetFieldByID(output, id, 0)
+			if err != nil {
+				logger.Errorf("failed to get resource property from ID: %s: %v", id, err)
+				return
+			}
+
+			// 25 define object from the resource property
+			helmChart = helm.HelmChart{FullName: chartName, Repo: helmRepo}
+		}
+		// 26 - get the list of kind
+		output, err := helmChart.ListNbChartKind(localFlag, "o1u", logger)
 		if err != nil {
-			logger.Errorf("failed to list helm repo: %v", err)
+			logger.Errorf("failed to list chart kind: %v", err)
 			return
 		}
-
-		// print the list
+		// print
 		list.PrettyPrintTable(output)
 
-		// Ask user which ID (to choose) from the printed list
-		id, err := ui.AskUserInt("\nchoose repo (enter ID): ")
-		if err != nil {
-			logger.Errorf("invalid ID: %v", err)
-			return
-		}
-
-		// define resource property from ID and output
-		repoName, err := list.GetFieldByID(output, id, 0)
-		if err != nil {
-			logger.Errorf("failed to get resource property from ID: %s: %v", id, err)
-			return
-		}
-
-		// define object from the resource property
-		helmRepo := helm.HelmRepo{Name: repoName}
-
-		// 22 - get the list of charts in this repo
-		output, err = helm.ListChart(localFlag, "o1u", helmRepo, logger)
-		if err != nil {
-			logger.Errorf("failed to list helm charts: %v", err)
-			return
-		}
-
-		// print the list
-		list.PrettyPrintTable(output)
-
-		// 23 - Ask user which ID (to choose) from the list
-		id, err = ui.AskUserInt("\nchoose chart (enter ID): ")
-		if err != nil {
-			logger.Errorf("invalid ID: %v", err)
-			return
-		}
-
-		// 24 - define resource property from ID and output
-		chartName, err := list.GetFieldByID(output, id, 0)
-		if err != nil {
-			logger.Errorf("failed to get resource property from ID: %s: %v", id, err)
-			return
-		}
-
-		// 25 define object from the resource property
-		helmChart := helm.HelmChart{FullName: chartName, Repo: helmRepo}
-
-		// 26 - get the list of chart kind
-		output, err = helm.ListChartKind(localFlag, "o1u", helmChart, logger)
+		// 27 - get the list of each kind
+		output, err = helmChart.ListChartKind(localFlag, "o1u", logger)
 		if err != nil {
 			logger.Errorf("failed to list chart kind: %v", err)
 			return
