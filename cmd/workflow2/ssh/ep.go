18f74b396c259bdbf8c9f561aae8ce91d72dcd84
abelgacem <amarbelgacem@hotmail.fr>
2025-11-07 14:34:29 +0100

    update

--- cmd/workflow2/ssh/ep.go
+++ cmd/workflow2/ssh/ep.go
@@ -4,7 +4,9 @@ Copyright © 2025 AB TRANSITION IT abtransitionit@hotmail.com
 package ssh
 
 import (
+	"github.com/abtransitionit/gocore/phase2"
 	common "github.com/abtransitionit/goluc/cmd/workflow2/_common"
+	"github.com/abtransitionit/gotask/mock/node"
 )
 
 // root Command
@@ -16,4 +18,12 @@ var EpCmd = common.GetEpCmd(
 func init() {
 	// sub cde
 	common.SetInitSubCmd(EpCmd, cmdPathName)
+
+	// get an instance of a FnRegistry
+	registry := phase2.GetFnRegistry()
+	// register function used
+
+	registry.Add("CheckSshConf", node.CheckSshConf)
+	registry.Add("CheckSshAccess", node.CheckSshAccess)
+
 }
