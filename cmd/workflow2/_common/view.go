7a8b39325ba91d883211d970cd97ab99bf1a3eda
abelgacem <amarbelgacem@hotmail.fr>
2025-11-07 15:13:52 +0100

    update

--- cmd/workflow2/_common/view.go
+++ cmd/workflow2/_common/view.go
@@ -5,6 +5,10 @@ package common
 
 import (
 	"fmt"
+	"path"
+	"reflect"
+	"runtime"
+	"strings"
 
 	"github.com/abtransitionit/gocore/list"
 	"github.com/abtransitionit/gocore/logx"
@@ -19,6 +23,7 @@ func GetViewCmd(cmdPathName string) *cobra.Command {
 		showConfigTable bool
 		showPhase       bool
 		showTier        bool
+		showFunction    bool
 	)
 
 	cobraCmd := &cobra.Command{
@@ -29,8 +34,8 @@ func GetViewCmd(cmdPathName string) *cobra.Command {
 			// define logger
 			logger := logx.GetLogger()
 
-			// at least one flag required
-			if !showConfigTxt && !showConfigTable && !showPhase && !showTier {
+			// at least one 	flag required
+			if !showConfigTxt && !showConfigTable && !showPhase && !showTier && !showFunction {
 				return fmt.Errorf("please specify one of: --config, --phase, or --tier")
 			}
 
@@ -99,6 +104,50 @@ func GetViewCmd(cmdPathName string) *cobra.Command {
 					logger.Infof("Workflow %s (Tier View) to %s", workflow.Name, workflow.Description)
 					list.PrettyPrintTable(tierView)
 				}
+
+			}
+
+			// --- PHASE VIEW---
+			if showFunction {
+				registry := phase2.GetFnRegistry()
+
+				keys := registry.List()
+
+				// Build the table as a raw TSV string:
+				// Header
+				var b strings.Builder
+				b.WriteString("Key\tPackage\tFunction\n")
+
+				for _, key := range keys {
+					fn, ok := registry.Get(key)
+					if !ok {
+						continue
+					}
+
+					fnVal := reflect.ValueOf(fn)
+					ptr := fnVal.Pointer()
+					rf := runtime.FuncForPC(ptr)
+
+					pkg := "<??>"
+					fnName := "<??>"
+
+					if rf != nil {
+						fullName := rf.Name() // github.com/.../node.CheckSshConf
+
+						// Remove prefix
+						prefix := "github.com/abtransitionit/"
+						fullName = strings.TrimPrefix(fullName, prefix)
+						// define var
+						pkg = path.Dir(fullName)
+						fnName = path.Base(fullName)
+					}
+
+					fmt.Fprintf(&b, "%s\t%s\t%s\n", key, pkg, fnName)
+				}
+
+				logger.Info("Function Registry (key + package + function)")
+
+				list.PrettyPrintTable(b.String())
 			}
 
 			// success
@@ -111,6 +160,7 @@ func GetViewCmd(cmdPathName string) *cobra.Command {
 	cobraCmd.Flags().BoolVarP(&showConfigTxt, "cfgTxt", "x", false, "Display workflow config file as a Txt file")
 	cobraCmd.Flags().BoolVarP(&showPhase, "wphase", "p", false, "Display workflow phases")
 	cobraCmd.Flags().BoolVarP(&showTier, "wtier", "t", false, "Display workflow tiers")
+	cobraCmd.Flags().BoolVarP(&showFunction, "func", "f", false, "Display workflow GO functions used and registered")
 
 	return cobraCmd
 }
