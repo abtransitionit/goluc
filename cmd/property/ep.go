793d6c35734ad0b9f162f606ea9fd9a357beab49
abelgacem <amarbelgacem@hotmail.fr>
2025-11-16 19:15:09 +0100

    update

--- cmd/property/ep.go
+++ cmd/property/ep.go
@@ -6,15 +6,16 @@ package property
 import (
 	"fmt"
 
-	linuxproperty "github.com/abtransitionit/golinux/property"
+	"github.com/abtransitionit/gocore/logx"
+	lproperty "github.com/abtransitionit/golinux/mock/property"
 	"github.com/spf13/cobra"
 )
 
-var remote string
-var vmName string
+var targetName string
+var propFlags = map[string]*bool{} // holds which flag was used - *string because some handlers need params
 
 // Description
-var SDesc = "Manage a property of an OS or a VM."
+var SDesc = "Get a property from a target machine (locally or remotely)."
 var LDesc = SDesc + "\n" + `
 This command allows to get several kind of OS properties like.
 
@@ -28,25 +29,57 @@ This command allows to get several kind of OS properties like.
 var EpCmd = &cobra.Command{
 	Use:   "property",
 	Short: SDesc,
-	// Args:  cobra.ExactArgs(1), // Require exactly one argument: the property name
-	Args: cobra.MinimumNArgs(1), // Require at least one argument: the property name
 	RunE: func(cmd *cobra.Command, args []string) error {
-		property := args[0]
-		params := args[1:] // slice of extra params
+		// define vars
+		var propertyName string
+		logger := logx.GetLogger()
 
-		var value string
-		var err error
-		value, err = linuxproperty.GetProperty(vmName, property, params...)
+		// 1 - get the selected property as flag
+		count := 0
+		for flag, ptr := range propFlags {
+			if *ptr {
+				propertyName = flag
+				count++
+			}
+		}
+		// 2 - check flag
+		if count == 0 {
+			// 21 - check missing flag
+			return fmt.Errorf("you must specify one property flag (e.g., --cpu, --path)")
+		} else if count > 1 {
+			// 22 - check mulmtiple flag
+			return fmt.Errorf("you must specify ONLY one property flag")
+		}
+
+		// 3 - get parameters if any
+		propertyParams := args
+
+		// log
+		// logger.Debugf("selected property: %s", property)
+		// logger.Debugf("params: %s", params)
+
+		// 4 - get property
+		value, err := lproperty.GetProperty(logger, targetName, propertyName, propertyParams...)
+		// 4 - handle system error
 		if err != nil {
 			return fmt.Errorf("%v", err)
 		}
+		// 5 - handle success
 		fmt.Println(value)
 		return nil
 	},
 }
 
 func init() {
-	// EpCmd.Flags().StringVarP(&remote, "remote", "r", "", "provide the name of the remote VM")
-	EpCmd.Flags().StringVar(&vmName, "vm", "", "VM name to query remotely")
+	// static flags
+	EpCmd.Flags().StringVarP(&targetName, "target", "t", "local", "Name of the target to query")
 
+	// dynamic flags - create automatically one bool flag per property
+	for prop := range lproperty.PropertyMap {
+		var flag bool
+		propFlags[prop] = &flag
+		EpCmd.Flags().BoolVar(propFlags[prop], prop, false, "")
+	}
 }
+
+// value, err = linuxproperty.GetProperty(targetName, property, params...)
