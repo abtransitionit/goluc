41262f1f3d683181815e90f95b637f96539ee7c0
abelgacem <amarbelgacem@hotmail.fr>
2025-12-20 18:00:48 +0100

    update

--- cmd/workflow2/kbe/kbe.go
+++ cmd/workflow2/kbe/kbe.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/abtransitionit/gocore/phase2"
 	"github.com/abtransitionit/gotask/mock/file"
+	"github.com/abtransitionit/gotask/mock/gopm"
+	"github.com/abtransitionit/gotask/mock/k8s"
 	"github.com/abtransitionit/gotask/mock/node"
 	"github.com/abtransitionit/gotask/mock/onpm"
 	"github.com/abtransitionit/gotask/mock/oskernel"
@@ -15,7 +17,7 @@ import (
 	"github.com/abtransitionit/gotask/mock/selinux"
 )
 
-// Package variables
+// register function used in the workflow
 var (
 	cmdPathName = "kbe"
 	shortDesc   = "create KBE (Kubernetes Easy) clusters."
@@ -24,24 +26,47 @@ var (
 // registred function
 func registerFunctions() {
 
-	// get an instance of the FnRegistry that is shared by all phases of a
-	registry := phase2.GetFnRegistry()
-	// get the workflow name
-	worflowName := filepath.Base(cmdPathName)
-	// register function used
+	// get objects
+	registry := phase2.GetFnRegistry()        // an instance of the FnRegistry that is shared by all phases
+	worflowName := filepath.Base(cmdPathName) // the workflow name
+
+	// code to ensure node status
+	registry.Add(worflowName, "CheckSshConf", node.CheckSshConf)
+	registry.Add(worflowName, "CheckSshAccess", node.CheckSshAccess)
+
+	// deploy an agent
+	registry.Add(worflowName, "DeployAgent", file.CopyFileWithSudo)
+
+	// create an RC file
+	registry.Add(worflowName, "CreateRcFile", file.Create)
+
+	// update RC file
+	registry.Add(worflowName, "UpdateRcFile", file.AddString)
+
+	// code to ensure OS is up to date
+	registry.Add(worflowName, "UpgradeOs", onpm.UpgradeOs)
+	registry.Add(worflowName, "UpdateOs", onpm.UpdateOs)
+	registry.Add(worflowName, "RebootIfNeeded", node.RebootIfNeeded)
+	registry.Add(worflowName, "WaitIsOnline", node.WaitIsSshOnline)
+
+	// code OS Native Package Manager specific
 	registry.Add(worflowName, "AddRepoNative", onpm.AddRepo)
 	registry.Add(worflowName, "AddPkgNative", onpm.AddPkg)
+
+	// code OS specific
 	registry.Add(worflowName, "AddKModule", oskernel.AddKModule)
 	registry.Add(worflowName, "AddKParam", oskernel.AddKParam)
 	registry.Add(worflowName, "EnableService", sys.Enable)
 	registry.Add(worflowName, "StartService", sys.Start)
-	registry.Add(worflowName, "CheckSshConf", node.CheckSshConf)
-	registry.Add(worflowName, "CheckSshAccess", node.CheckSshAccess)
-	registry.Add(worflowName, "DeployAgent", file.CopyFileWithSudo)
-	registry.Add(worflowName, "RebootIfNeeded", node.RebootIfNeeded)
-	registry.Add(worflowName, "RebootIfNeeded", node.RebootIfNeeded)
 	registry.Add(worflowName, "SelinuxConfigure", selinux.ConfigureSelinux)
-	registry.Add(worflowName, "UpgradeOs", onpm.UpgradeOs)
-	registry.Add(worflowName, "UpdateOs", onpm.UpdateOs)
-	registry.Add(worflowName, "WaitIsOnline", node.WaitIsSshOnline)
+
+	// code K8s specific
+	registry.Add(worflowName, "ResetNode", k8s.ResetNode)
+	registry.Add(worflowName, "InitCplane", k8s.InitCplane)
+	registry.Add(worflowName, "AddWorker", k8s.AddWorker)
+	registry.Add(worflowName, "ConfigureKubectl", k8s.ConfigureKubectl)
+	registry.Add(worflowName, "InstallGoCli", gopm.InstallGoCli)
+	registry.Add(worflowName, "AddRepoHelm", k8s.AddRepoHelm)
+	registry.Add(worflowName, "AddPkgHelm", k8s.AddChartHelm)
+
 }
