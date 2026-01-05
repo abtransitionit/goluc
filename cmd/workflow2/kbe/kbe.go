c8a7ad52da22726a40f7e4f9e407125ea3806dcc
abelgacem <amarbelgacem@hotmail.fr>
2026-01-05 14:13:27 +0100

    update

--- cmd/workflow2/kbe/kbe.go
+++ cmd/workflow2/kbe/kbe.go
@@ -30,11 +30,11 @@ func registerFunctions() {
 	registry := phase2.GetFnRegistry()        // an instance of the FnRegistry that is shared by all phases
 	worflowName := filepath.Base(cmdPathName) // the workflow name
 
-	// code to ensure node:ssh status
+	// ensure node:ssh status
 	registry.Add(worflowName, "CheckSshConf", node.CheckSshConf)
 	registry.Add(worflowName, "CheckSshAccess", node.CheckSshAccess)
 
-	// code to ensure node:OS is up to date
+	// ensure node:OS is up to date
 	registry.Add(worflowName, "UpgradeOs", onpm.UpgradeOs)
 	registry.Add(worflowName, "UpdateOs", onpm.UpdateOs)
 	registry.Add(worflowName, "RebootIfNeeded", node.RebootIfNeeded)
@@ -43,28 +43,36 @@ func registerFunctions() {
 	// deploy an agent
 	registry.Add(worflowName, "DeployAgent", file.CopyFileWithSudo)
 
-	// mange RC file
+	// mange Linux RC file
 	registry.Add(worflowName, "CreateRcFile", file.CreateRcFile)
 	registry.Add(worflowName, "RcAddPath", file.RcAddPath)
 	// registry.Add(worflowName, "UpdateRcFile", file.AddString)
 
-	// code to manage Linux OS Native Package
+	// manage Linux OS Native Package
 	registry.Add(worflowName, "AddRepoNative", onpm.AddRepo)
 	registry.Add(worflowName, "AddPkgNative", onpm.AddPkg)
 
-	// code Linux OS specific
+	// manage Go Package
+	registry.Add(worflowName, "AddPkgGo", gopm.AddPkgGo)
+
+	// manage Linux OS kernel
 	registry.Add(worflowName, "AddKModule", oskernel.LoadModule)
 	registry.Add(worflowName, "AddKParam", oskernel.LoadParam)
+
+	// manage Linux service
 	registry.Add(worflowName, "EnableService", sys.Enable)
 	registry.Add(worflowName, "StartService", sys.Start)
+
+	// manage Linux Selinux
 	registry.Add(worflowName, "SelinuxConfigure", selinux.Configure)
 
-	// code K8s specific
+	// manage kubernetes cluster
 	registry.Add(worflowName, "ResetNode", k8s.ResetNode)
 	registry.Add(worflowName, "InitCplane", k8s.InitCplane)
 	registry.Add(worflowName, "AddWorker", k8s.AddWorker)
+	// - kubectl
 	registry.Add(worflowName, "ConfigureKubectl", k8s.ConfigureKubectl)
-	registry.Add(worflowName, "AddPkgGo", gopm.AddPkgGo)
+	// - Helm
 	registry.Add(worflowName, "AddRepoHelm", k8s.AddRepoHelm)
 	registry.Add(worflowName, "AddPkgHelm", k8s.AddChartHelm)
 
